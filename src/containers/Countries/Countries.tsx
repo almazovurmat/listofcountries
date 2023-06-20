e58e893a767240c7dfb8bdd8495fdc7e85f304f5
Almazov Urmat <almazov.urmat@gmail.com>
2023-06-20 23:08:59 +0600

    created container Countries

---
+++ src/containers/Countries/Countries.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import CountriesList from "../../components/CountriesList/CountriesList";
+
+
+const Countries = () => {
+
+    return (
+        <div className="countries">
+            <CountriesList />
+        </div>
+    );
+};
+
+export default Countries;
